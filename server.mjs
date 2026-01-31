8fe881978f66f089e505501fe12f49da7b77d4ca
Doldawy <230869851+Doldawy@users.noreply.github.com>
2026-01-31 19:54:50 +0300

    Add server.mjs to create a basic HTTP server

---
+++ server.mjs
@@ -0,0 +1,11 @@
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Railway!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
